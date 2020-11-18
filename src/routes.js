6ccc69f76d6304a28502f4fcfcf213c6705ce767
Francesco Esposito <francesco.esposito@biotecnomed.it>
2020-11-18 15:47:20 +0100

    All Css Zero

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import Dashboard from "views/Dashboard.jsx";
 import UserProfile from "views/UserProfile.jsx";
 import TableList from "views/TableList.jsx";
 import Typography from "views/Typography.jsx";
-import Maps from "views/Maps.jsx";
-import Notifications from "views/Notifications.jsx";
 
 
 const dashboardRoutes = [
@@ -33,21 +31,6 @@ const dashboardRoutes = [
     name: "Typography",
     icon: "pe-7s-news-paper",
     component: Typography,
-    layout: "/admin"
-  },
-
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "pe-7s-map-marker",
-    component: Maps,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "pe-7s-bell",
-    component: Notifications,
     layout: "/admin"
   }
 ];
