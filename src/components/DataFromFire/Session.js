44238d1c4020b2b4f4af2d96166af66661a2d542
Francesco Esposito <francesco.esposito@biotecnomed.it>
2020-12-09 18:10:31 +0100

    update pomeriggio

--- src/components/DataFromFire/Session.js
+++ src/components/DataFromFire/Session.js
@@ -1,39 +1,46 @@
 import { UserCard } from "components/UserCard/UserCard.jsx";
 import React, { Component } from 'react'
 import avatar from "assets/img/faces/face-1.jpg";
-
+// import ChartistGraph from "react-chartist";
 import Fire from 'firebase'
 
+import Moment from 'react-moment'
+import 'moment-timezone'
+
 export class Session extends Component {
-
-
     constructor(props) {
         super()
         this.state = {
-            session: []
+            info: []
 
         }
     }
-    componentWillMount() {
-        this.getSession()
-    }
 
-    getSession() {
+    componentDidMount() {
+
         var session = [];
         Fire.database().ref("sessions/" + this.props.history.location.state.keySess).on('value', snapshot => {
-            // session.push(snapshot.val().events)
-            // console.log(snapshot.val().events)
-            snapshot.forEach(snap => {
-                session.push(snap.val())
-                    // console.log(snap.val())
+            // console.log(snapshot.toJSON().events)
+            session.push({
+                complete: snapshot.toJSON().complete,
+                end: snapshot.toJSON().end_time.time,
+                start: snapshot.toJSON().start_time.time,
+                events: snapshot.toJSON().events,
+                tests: snapshot.toJSON().tests
             })
-            // console.log(session)
 
-            this.setState({ session: session[2] });
-            console.log(this.state.session)
+            // snapshot.forEach(snap => {
+            //     // console.log(snap.toJSON())
 
+            //     session.push(snap.toJSON());
+            // })
+            console.log(session[0].start)
+            this.setState({ info: session[0] })
         })
     }
+
+
+
 
     render() {
         return (
@@ -51,17 +58,22 @@ export class Session extends Component {
                     />
                 </div>
 
-                <div className='content'>
+                <div>
                     <h1>
+                        <p>{this.state.info.end}</p>
+                    <Moment tz='Europe/Rome' format="DD/MM/YY hh:mm">{this.state.info.start}</Moment>
 
-Azzippa la tab
                     </h1>
 
                 </div>
-            </div>
+            </div >
 
         )
     }
 }
 
 export default Session
+
+
+
+
